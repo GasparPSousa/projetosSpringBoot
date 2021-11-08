8413fd41f207495a04ac4f1062ccc5c9d195b928
GasparPSousa <gasparufrj@gmail.com>
2021-11-08 16:37:05 -0300

    Criando um projeto Spring Boot com as dependencias Spring Web, Spring Data JPA, H2 Database e PostgreSQL Driver

---
+++ aula017/projetoReal/src/main/java/br/com/dh/projetoReal/ProjetoRealApplication.java
@@ -0,0 +1,13 @@
+package br.com.dh.projetoReal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRealApplication.class, args);
+	}
+
+}
---
+++ aula017/projetoReal/src/test/java/br/com/dh/projetoReal/ProjetoRealApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.projetoReal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
