5054519ffe7b9c9541fc0c6931e4d8ed53965345
GasparPSousa <gasparufrj@gmail.com>
2021-11-12 09:34:38 -0300

    Projeto Inicial - Idade

---
+++ aula021/src/main/java/br/com/dh/aula021/Aula021Application.java
@@ -0,0 +1,13 @@
+package br.com.dh.aula021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula021Application.class, args);
+	}
+
+}
---
+++ aula021/src/test/java/br/com/dh/aula021/Aula021ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.aula021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
