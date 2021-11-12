b4e6baac3fc7a3d1160bfde2f0a7e189980057f4
GasparPSousa <gasparufrj@gmail.com>
2021-11-11 23:31:31 -0300

    Projeto Inicial

---
+++ aula20/aula20/src/test/java/com/dh/aula20/Aula20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.aula20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
