d80e106a17bed581da3e670911926b8d638f9397
GasparPSousa <gasparufrj@gmail.com>
2021-11-21 17:43:03 -0300

    Criando a classe Util

---
+++ project/src/main/java/com/example/demo/Util/Util.java
@@ -0,0 +1,12 @@
+package com.example.demo.Util;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Util {
+
+    public static Timestamp dateToTimestamp(Date date) {
+        Timestamp timestamp = new Timestamp(date.getTime());
+        return timestamp;
+    }
+}
