81342d6f3b50a23aab991bc6f251e994e975ec5b
GasparPSousa <gasparufrj@gmail.com>
2021-12-01 09:05:15 -0300

    Projeto Inicial

---
+++ aula34-mongoDB/src/main/java/com/dh/aula34mongoDB/Aula34MongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.dh.aula34mongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula34MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula34MongoDbApplication.class, args);
+	}
+
+}
---
+++ aula34-mongoDB/src/test/java/com/dh/aula34mongoDB/Aula34MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.aula34mongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula34MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
